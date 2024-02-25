dfd2259ff89ffcfcb1cc8288a42106317a12491b
Sai Geetha <144041387+simpleithub@users.noreply.github.com>
2024-02-25 18:21:22 +0530

    Add files via upload

---
+++ Recurssion/fibonacci.py
@@ -0,0 +1,11 @@
+#fibonacci by recurssion 
+def fibonacciOfN(n):
+    if n == 0 :
+        return 0
+    elif n == 1 or n ==2 :
+        return 1
+    else :
+        return fibonacci(n-1) + fibonacci(n-2)    
+    
+print(fibonacci(5))   
+    
