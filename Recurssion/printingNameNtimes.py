847fb0cbba748b38d8d2a912d888428f97377101
Sai Geetha <144041387+simpleithub@users.noreply.github.com>
2024-02-25 11:10:03 +0530

    Update printingNameNtimes.py

--- Recurssion/printingNameNtimes.py
+++ Recurssion/printingNameNtimes.py
@@ -1,1 +1,13 @@
+#print name for a particular no.of.times by recurssion
 
+def printNameNtimes(name,n,c=0): 
+    if c >= n :
+        return 
+    else :
+        print(name)
+        c+=1
+        return printNameNtimes(name , n ,c)
+
+x = input("enter ur name :")
+n = int(input("enter count :"))
+printNameNtimes(x,n)       
