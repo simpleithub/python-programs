5e6d8e45b028d37ce1abcb781a9eb62c85b5c6ce
Sai Geetha <144041387+simpleithub@users.noreply.github.com>
2024-03-01 19:04:05 +0530

    Update right_rotate.py

--- arrays/right_rotate.py
+++ arrays/right_rotate.py
@@ -14,3 +14,25 @@ arr = [1,2,3,4,5]
 
 print(right_rotate(arr))
 
+
+#SOLUTION-2
+#right rotate optimal method
+def reverse(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+  
+
+def rotate_array(arr,d) :
+    n = len(arr)
+    d = d % n
+    reverse(arr,0,n-1)
+    reverse(arr,d,n-1)
+    reverse(arr,0,d-1)
+    
+    
+arr = [1,2,3,4,5,6,7]    
+d = 1
+rotate_array(arr,d)
+print(arr)
